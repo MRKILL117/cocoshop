3e109d415807970110c49470839d1fac2221ce6d
H3lltronik <esau.egs1@gmail.com>
2019-04-02 23:42:14 -0600

    Esto es de un tuto, no le hagan caso, me pidio commit para el pull

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import LoginComponent from './components/inicio/login.vue'
+import Bienvenido from './components/inicio/bienvenida.vue'
+
+Vue.component('login-component', LoginComponent)
+Vue.component('bienvenida-component', Bienvenido)
+
 Vue.config.productionTip = false
 
 new Vue({

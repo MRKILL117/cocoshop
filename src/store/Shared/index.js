508a1858e79579ed89ae12338d594bd560edfab2
Tectoc <tecotoc@gmail.com>
2019-05-10 23:03:17 -0500

    Ya esta mi parte

--- src/store/Shared/index.js
+++ src/store/Shared/index.js
@@ -8,7 +8,7 @@ Vue.use(Vuex)
 export default({
   state: {
       cargando: false,
-      urlBase: 'http://localhost:81/Cocoshop/'
+      urlBase: 'http://localhost/Cocoshop/'
   },
   mutations: {
       setCargando (state, value) {
